2fdb14bdad51c338ce41d15619772467c958ece4
Lino Alves <lino.alves@liferay.com>
2018-03-20 19:01:42 -0300

    demo aws comprehend

---
+++ src/main/java/com/text/demo/DemoApplication.java
@@ -0,0 +1,49 @@
+package com.text.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.services.comprehend.AmazonComprehend;
+import com.amazonaws.services.comprehend.AmazonComprehendClientBuilder;
+import com.amazonaws.services.comprehend.model.DetectSentimentRequest;
+import com.amazonaws.services.comprehend.model.DetectSentimentResult;
+
+@RestController
+@EnableAutoConfiguration
+@SpringBootApplication
+public class DemoApplication {
+
+	@RequestMapping("/feel")
+	String feel(@RequestParam("text") String text) {
+
+        AWSCredentialsProvider awsCreds = DefaultAWSCredentialsProviderChain.getInstance();
+ 
+        AmazonComprehend comprehendClient =
+            AmazonComprehendClientBuilder.standard()
+                                         .withCredentials(awsCreds)
+                                         .withRegion("us-east-1")
+                                         .build();
+                                         
+        DetectSentimentRequest detectSentimentRequest = new DetectSentimentRequest().withText(text)
+                                                                                    .withLanguageCode("en");
+        DetectSentimentResult detectSentimentResult = comprehendClient.detectSentiment(detectSentimentRequest);
+		
+        return detectSentimentResult.toString() + "\n";
+	}
+	
+	@RequestMapping("/")
+	String home() {
+		
+		return "Hello!";
+    }
+	
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
